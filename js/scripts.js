e9113ffddfb707f9cf3c006d266bfdb2a35b7561
jjtrosclair <57603010+jjtrosclair@users.noreply.github.com>
2020-06-24 13:43:39 -0400

    added first three pokemon to list array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
-alert('Hello world');
-var favoriteFood = "Tuna Steak";
-document.write(favoriteFood);
+var pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: .7,
+        types: ['grass', 'poison'],
+        abilities: ['Chlorophyll', 'Overgrow']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        types: ['fire'],
+        abilities: ['Blaze', 'Solar-power']
+    },
+    {
+        name: 'Squirtle',
+        height: .5,
+        types: ['water'],
+        abilities: ['Rain-dish', 'Torrent']
+    },
+]
 
-
-
+console.log(pokemonList);
